8a3f761f4ef21af36a33130d3b4199aebd2140c7
Matze149 <93557310+Matze149@users.noreply.github.com>
2025-02-10 19:48:16 +0100

    Logic to load Avatars & Drawings from a dictionary

--- scenes/space/CharmSummoner.cs
+++ scenes/space/CharmSummoner.cs
@@ -7,10 +7,10 @@ public partial class CharmSummoner : Node3D
 	[Export] private int Amount  { get; set; } = 100;
 	[Export] private int PositionRange { get; set; } = 20;
 	[Export] private float MinDistance { get; set; } = 3.0f;
-    [Export] private Godot.Collections.Array<Texture2D> Avatars { get; set; }
+	//[Export] private Godot.Collections.Array<Texture2D> Avatars { get; set; }
 	[Export] private string ResponsesPath { get; set; }
-    [Export] private Godot.Collections.Array<Texture2D> BodiesBase { get; set; }
-    [Export] private Godot.Collections.Array<Texture2D> BodiesHeart { get; set; } // indices must match base
+	[Export] private Godot.Collections.Array<Texture2D> BodiesBase { get; set; }
+	[Export] private Godot.Collections.Array<Texture2D> BodiesHeart { get; set; } // indices must match base
 
 	private float HalfRange => PositionRange / 2;
 	private Vector3[] GeneratedPosition { get; set; } = [];
@@ -34,15 +34,51 @@ public partial class CharmSummoner : Node3D
 			GD.PrintErr("Base body and heart eye variant counts do not match up!");
 		}
 
-        GodotObject fileResource = ResourceLoader.Load(ResponsesPath);
-        var responsesArray = (Godot.Collections.Array)fileResource.Get("records");
+		GodotObject fileResource = ResourceLoader.Load(ResponsesPath);
+		var responsesArray = (Godot.Collections.Array)fileResource.Get("records");
 
-        Godot.Collections.Array<int> indexDeck = [];
+		Godot.Collections.Array<int> indexDeck = [];
 		for (int i = 0; i < BodiesBase.Count; ++i) {
 			indexDeck.Add(i);
 		}
 
-        for (int i = 0; i < responsesArray.Count; i++)
+		string basePathAvatars = "res://..//..//assets//textures//Valentines2025//Avatars//";		
+		Godot.Collections.Dictionary Avatars = new Godot.Collections.Dictionary {
+			{ "Alca", ResourceLoader.Load(basePathAvatars + "Alca.jpg") },
+			{ "ArchiveAnon", ResourceLoader.Load(basePathAvatars + "ArchiveAnon.jpeg") },
+			{ "Boyong", ResourceLoader.Load(basePathAvatars + "Boyong.png") },
+			{ "Doko", ResourceLoader.Load(basePathAvatars + "Doko.png") },
+			{ "DWraith23", ResourceLoader.Load(basePathAvatars + "DWraith23.png") },
+			{ "Endor", ResourceLoader.Load(basePathAvatars + "Endor.png") },
+			{ "Firebreath", ResourceLoader.Load(basePathAvatars + "Firebreath.png") },
+			{ "Floran", ResourceLoader.Load(basePathAvatars + "Floran.png") },
+			{ "GerardoS23", ResourceLoader.Load(basePathAvatars + "GerardoS23.png") },
+			{ "Joseku", ResourceLoader.Load(basePathAvatars + "Joseku.png") },
+			{ "Konsulus", ResourceLoader.Load(basePathAvatars + "Konsulus.png") },
+			{ "Koudelka", ResourceLoader.Load(basePathAvatars + "Koudelka.jpeg") },
+			{ "Maedara", ResourceLoader.Load(basePathAvatars + "Maedara.png") },
+			{ "Necrozma", ResourceLoader.Load(basePathAvatars + "Necrozma.png") },
+			{ "Neon", ResourceLoader.Load(basePathAvatars + "Neon.jpeg") },
+			{ "SleepingSlime", ResourceLoader.Load(basePathAvatars + "SleepingSlime.png") }
+		};
+
+		string basePathDrawings = "res://..//..//assets//textures//Valentines2025//Drawings//";		
+		Godot.Collections.Dictionary Drawings = new Godot.Collections.Dictionary {
+			{ "Alca", ResourceLoader.Load(basePathDrawings + "Alca.jpg") },
+			{ "ArchiveAnon", ResourceLoader.Load(basePathDrawings + "ArchiveAnon.jpeg") },
+			{ "Doko", ResourceLoader.Load(basePathDrawings + "Doko.png") },
+			{ "DWraith23", ResourceLoader.Load(basePathDrawings + "DWraith23.png") },
+			{ "Endor", ResourceLoader.Load(basePathDrawings + "Endor.png") },
+			{ "Firebreath", ResourceLoader.Load(basePathDrawings + "Firebreath.png") },
+			{ "Joseku", ResourceLoader.Load(basePathDrawings + "Joseku.png") },
+			{ "Koudelka", ResourceLoader.Load(basePathDrawings + "Koudelka.jpeg") },
+			{ "Maedara", ResourceLoader.Load(basePathDrawings + "Maedara.png") },
+			{ "Necrozma", ResourceLoader.Load(basePathDrawings + "Necrozma.png") },
+			{ "Neon", ResourceLoader.Load(basePathDrawings + "Neon.jpeg") },
+			{ "Streamcrash", ResourceLoader.Load(basePathDrawings + "Streamcrash.jpeg") }
+		};
+
+		for (int i = 0; i < responsesArray.Count; i++)
 		{
 			var charm = LuckyCharm.GenerateInstance();
 			charm.Camera = GetParent().FindChild("Mitty Cam") as MittyCam;
@@ -57,16 +93,22 @@ public partial class CharmSummoner : Node3D
 			{
 				charm.CharmName = responsesArray[i].AsStringArray()[1];
 			}
-            charm.CharmMessage = responsesArray[i].AsStringArray()[5];
+			charm.CharmMessage = responsesArray[i].AsStringArray()[5];
 
-            // Shuffle all bodies, then draw one until none remain, shuffle and go again
-            // aka Tetris piece selection logic
-            int deckPos = i % BodiesBase.Count;
-            if (deckPos == 0) {
+			// Shuffle all bodies, then draw one until none remain, shuffle and go again
+			// aka Tetris piece selection logic
+			int deckPos = i % BodiesBase.Count;
+			if (deckPos == 0) {
 				indexDeck.Shuffle();
 			}
-            // charm.Avatar = Avatars[GD.RandRange(0, Avatars.Length - 1)];
-            charm.BodyTexture = BodiesBase[indexDeck[deckPos]];
+			
+			if(Avatars.ContainsKey(responsesArray[i].AsStringArray()[1]))
+				charm.Avatar = (Texture2D)Avatars[responsesArray[i].AsStringArray()[1]];
+			
+			if(Drawings.ContainsKey(responsesArray[i].AsStringArray()[1]))
+				charm.Drawing = (Texture2D)Drawings[responsesArray[i].AsStringArray()[1]];
+			
+			charm.BodyTexture = BodiesBase[indexDeck[deckPos]];
 			charm.BodyTextureHeart = BodiesHeart[indexDeck[deckPos]];
 
 			charm.Position = MakeRandomPlanetPosition();
@@ -79,16 +121,16 @@ public partial class CharmSummoner : Node3D
 
 	private Vector3 MakeRandomPlanetPosition()
 	{
-        var theta = GD.RandRange(0, Math.PI * 2);
-        var phi = GD.RandRange(0, Math.PI); // I"M NOT GREEK!  THE FUCK DOES THIS MEAN? // Theta and phi are traditional variable names for angles in math equations.
-        var radius = 25f;       // WHAT THE FUCK WAS THIS BULLSHIT?! // This was basically rolling the latitude and longitude and then adjusting the location for how big the planet is.
+		var theta = GD.RandRange(0, Math.PI * 2);
+		var phi = GD.RandRange(0, Math.PI); // I"M NOT GREEK!  THE FUCK DOES THIS MEAN? // Theta and phi are traditional variable names for angles in math equations.
+		var radius = 25f;       // WHAT THE FUCK WAS THIS BULLSHIT?! // This was basically rolling the latitude and longitude and then adjusting the location for how big the planet is.
 
-        var x = (float)(radius * Math.Sin(theta) * Math.Cos(phi));
-        var y = (float)(radius * Math.Sin(theta) * Math.Sin(phi));
-        var z = (float)(radius * Math.Cos(theta));
+		var x = (float)(radius * Math.Sin(theta) * Math.Cos(phi));
+		var y = (float)(radius * Math.Sin(theta) * Math.Sin(phi));
+		var z = (float)(radius * Math.Cos(theta));
 
 		return new Vector3(x, y, z);
-    }
+	}
 
 	// func _ready():
 	// for i in range(amount):
--- scenes/space/LuckyCharm.cs
+++ scenes/space/LuckyCharm.cs
@@ -3,296 +3,297 @@ using System;
 
 public partial class LuckyCharm : CharacterBody3D
 {
-    public static LuckyCharm GenerateInstance() => GD.Load<PackedScene>("res://scenes/space/lucky_charm.tscn").Instantiate<LuckyCharm>();
+	public static LuckyCharm GenerateInstance() => GD.Load<PackedScene>("res://scenes/space/lucky_charm.tscn").Instantiate<LuckyCharm>();
 
-    #region Signals and Exports
-    [Signal] public delegate void SelectedEventHandler(LuckyCharm charm);
-    [Signal] public delegate void DeselectedEventHandler();
-    [Signal] public delegate void UnsnappingEventHandler();
-    [Signal] public delegate void DeselectStartedEventHandler();
-    [Signal] public delegate void HoverEnteredEventHandler();
-    [Signal] public delegate void HoverExitedEventHandler();
-
-
-    [ExportGroup("Nodes")]
-    [Export] private NavigationAgent3D Navigation { get; set; }
-    [Export] private MeshInstance3D Body { get; set; }
-    [Export] private CollisionShape3D Collider { get; set; }
-    [Export] private VisibleOnScreenNotifier3D VisibilityChecker { get; set; }
-    [Export] private Node3D MessageLabel { get; set; }
-    [Export] private Node3D MiniLabel { get; set; }
-    [ExportGroup("Properties")]
-    [ExportSubgroup("Wander")]
-    [Export] private float WanderRange { get; set; } = 10f;
-    [Export] private float Acceleration { get; set; } = 1f;
-    [Export] private float Speed { get; set; } = 5.0f;
-    [ExportSubgroup("Jump")]
-    [Export] private Vector2 JumpIntervalMS { get; set; } = new(2000, 3000);
-    [Export] private Vector2 JumpHeight { get; set; } = new(0.2f, 0.4f);
-    [Export] private Vector2 JumpDurationMS { get; set; } = new(800, 1200);
-
-    #endregion
-
-    #region Properties
-    // Camera
-    public MittyCam Camera { get; set; }
-    public Control OrbitControls { get; set; }
-    private float CameraSnapDurationMS { get; set; } = 5000.0f;
-    private Transform3D CameraSnapTransform => MessageLabel.GlobalTransform.TranslatedLocal(new Vector3(1.0f, 0.0f, 3.5f));
-    private int CameraSnapStartedMS { get; set; }
-    private Transform3D CameraUnsnapTransform { get; set; }
-    private int CameraUnsnapStartedMS { get; set; }
-
-    // Behavior
-    public Marker3D Pivoter { get; set; }
-    private int NextJumpMS { get; set; }
-    private Tween JumpTween { get; set; }
-    private Vector3 Destination { get; set; }
-
-    // Attributes
-    public Texture2D Avatar { get; set; }
-    public string CharmName { get; set; }
-    public string CharmMessage { get; set; }
-    public Texture2D BodyTexture { get; set; }
-    public Texture2D BodyTextureHeart { get; set; }
-
-    // State
-    public bool IsSelected { get; set; } = false;
-    public bool SelectionDisabled { get; set; } = false;
-    public bool IsOnScreen { get; set; } = false;
-    public bool IsFleeing { get; set; } = false;
-    public bool HasBeenFound { get; set; } = false;
-
-    #endregion
-
-    public override void _Ready()
-    {
-        // MessageLabel.Set("camera", Camera);
-        // MiniLabel.Set("camera", Camera);
-
-        // var bigLabelAvatar = MessageLabel.FindChild("Avatar") as MeshInstance3D;
-        // var smallLabelAvatar = MiniLabel.FindChild("Avatar") as MeshInstance3D;
-
-        // var material = bigLabelAvatar.Mesh.SurfaceGetMaterial(0).Duplicate(true) as StandardMaterial3D;
-        // material.AlbedoTexture = Avatar;
-        // bigLabelAvatar.MaterialOverride = material;
-
-        // var material2 = smallLabelAvatar.Mesh.SurfaceGetMaterial(0).Duplicate(true) as StandardMaterial3D;
-        // material2.AlbedoTexture = Avatar;
-        // smallLabelAvatar.MaterialOverride = material2;
-
-        ApplyBodyTexture(BodyTexture);
-    }
-
-    public override void _PhysicsProcess(double delta)
-    {
-        
-        Vector3 velocity = Velocity;
-
-        if (Camera != null)
-        {   var spaceState = GetWorld3D().DirectSpaceState;
-            var result = spaceState.IntersectRay(
-                new PhysicsRayQueryParameters3D()
-                {
-                    From = GlobalPosition,
-                    To = Camera.GlobalPosition,
-                    CollideWithAreas = true,
-                    CollideWithBodies = true,
-                }
-            );
-
-            if (result.Count > 0 || !VisibilityChecker.IsOnScreen())
-            {
-                Undetected();
-            }
-            else
-            {
-                Detected();
-            }
-        }
-
-        // Add the gravity.
-        if (!IsOnFloor())
-        {
-            velocity += GetGravity() * (float)delta;
-        }
-
-        Wander();
-
-        // var direction = ($NavigationAgent3D.get_next_path_position() - global_position).normalized()
-        // velocity = velocity.lerp(direction * speed, accel * delta)
-
-        var direction = (Navigation.GetNextPathPosition() - GlobalPosition).Normalized();
-        Velocity = Velocity.Lerp(direction * Speed, (float)(Acceleration * delta));
-
-        MoveAndSlide();
-
-        // Jump();
-    }
+	#region Signals and Exports
+	[Signal] public delegate void SelectedEventHandler(LuckyCharm charm);
+	[Signal] public delegate void DeselectedEventHandler();
+	[Signal] public delegate void UnsnappingEventHandler();
+	[Signal] public delegate void DeselectStartedEventHandler();
+	[Signal] public delegate void HoverEnteredEventHandler();
+	[Signal] public delegate void HoverExitedEventHandler();
 
 
-    private void OnBodyMouseEntered()
-    {
-        if (SelectionDisabled) return;
+	[ExportGroup("Nodes")]
+	[Export] private NavigationAgent3D Navigation { get; set; }
+	[Export] private MeshInstance3D Body { get; set; }
+	[Export] private CollisionShape3D Collider { get; set; }
+	[Export] private VisibleOnScreenNotifier3D VisibilityChecker { get; set; }
+	[Export] private Node3D MessageLabel { get; set; }
+	[Export] private Node3D MiniLabel { get; set; }
+	[ExportGroup("Properties")]
+	[ExportSubgroup("Wander")]
+	[Export] private float WanderRange { get; set; } = 10f;
+	[Export] private float Acceleration { get; set; } = 1f;
+	[Export] private float Speed { get; set; } = 5.0f;
+	[ExportSubgroup("Jump")]
+	[Export] private Vector2 JumpIntervalMS { get; set; } = new(2000, 3000);
+	[Export] private Vector2 JumpHeight { get; set; } = new(0.2f, 0.4f);
+	[Export] private Vector2 JumpDurationMS { get; set; } = new(800, 1200);
 
-        Input.SetDefaultCursorShape(Input.CursorShape.PointingHand);
-        EmitSignal(SignalName.HoverEntered);
-    }
+	#endregion
 
-    private void OnBodyMouseExited()
-    {
-        if (SelectionDisabled) return;
+	#region Properties
+	// Camera
+	public MittyCam Camera { get; set; }
+	public Control OrbitControls { get; set; }
+	private float CameraSnapDurationMS { get; set; } = 5000.0f;
+	private Transform3D CameraSnapTransform => MessageLabel.GlobalTransform.TranslatedLocal(new Vector3(1.0f, 0.0f, 3.5f));
+	private int CameraSnapStartedMS { get; set; }
+	private Transform3D CameraUnsnapTransform { get; set; }
+	private int CameraUnsnapStartedMS { get; set; }
 
-        Input.SetDefaultCursorShape(Input.CursorShape.Arrow);
-        EmitSignal(SignalName.HoverExited);
-    }
+	// Behavior
+	public Marker3D Pivoter { get; set; }
+	private int NextJumpMS { get; set; }
+	private Tween JumpTween { get; set; }
+	private Vector3 Destination { get; set; }
 
-    public override void _Input(InputEvent @event)
-    {
-        if (@event is not InputEventMouseButton mouse || mouse.ButtonMask == MouseButtonMask.Left) return;
-        if (SelectionDisabled) return;
+	// Attributes
+	public Texture2D Avatar { get; set; }
+	public Texture2D Drawing { get; set; }
+	public string CharmName { get; set; }
+	public string CharmMessage { get; set; }
+	public Texture2D BodyTexture { get; set; }
+	public Texture2D BodyTextureHeart { get; set; }
 
-        if (IsSelected)
-        {
-            CameraUnsnapStartedMS = (int)Time.GetTicksMsec();
-            EmitSignal(SignalName.Unsnapping);
-        }
-    }
+	// State
+	public bool IsSelected { get; set; } = false;
+	public bool SelectionDisabled { get; set; } = false;
+	public bool IsOnScreen { get; set; } = false;
+	public bool IsFleeing { get; set; } = false;
+	public bool HasBeenFound { get; set; } = false;
+
+	#endregion
+
+	public override void _Ready()
+	{
+		// MessageLabel.Set("camera", Camera);
+		// MiniLabel.Set("camera", Camera);
+
+		// var bigLabelAvatar = MessageLabel.FindChild("Avatar") as MeshInstance3D;
+		// var smallLabelAvatar = MiniLabel.FindChild("Avatar") as MeshInstance3D;
+
+		// var material = bigLabelAvatar.Mesh.SurfaceGetMaterial(0).Duplicate(true) as StandardMaterial3D;
+		// material.AlbedoTexture = Avatar;
+		// bigLabelAvatar.MaterialOverride = material;
+
+		// var material2 = smallLabelAvatar.Mesh.SurfaceGetMaterial(0).Duplicate(true) as StandardMaterial3D;
+		// material2.AlbedoTexture = Avatar;
+		// smallLabelAvatar.MaterialOverride = material2;
+
+		ApplyBodyTexture(BodyTexture);
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		
+		Vector3 velocity = Velocity;
+
+		if (Camera != null)
+		{   var spaceState = GetWorld3D().DirectSpaceState;
+			var result = spaceState.IntersectRay(
+				new PhysicsRayQueryParameters3D()
+				{
+					From = GlobalPosition,
+					To = Camera.GlobalPosition,
+					CollideWithAreas = true,
+					CollideWithBodies = true,
+				}
+			);
+
+			if (result.Count > 0 || !VisibilityChecker.IsOnScreen())
+			{
+				Undetected();
+			}
+			else
+			{
+				Detected();
+			}
+		}
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		Wander();
+
+		// var direction = ($NavigationAgent3D.get_next_path_position() - global_position).normalized()
+		// velocity = velocity.lerp(direction * speed, accel * delta)
+
+		var direction = (Navigation.GetNextPathPosition() - GlobalPosition).Normalized();
+		Velocity = Velocity.Lerp(direction * Speed, (float)(Acceleration * delta));
+
+		MoveAndSlide();
+
+		// Jump();
+	}
 
 
-    private void OnBodyInputEvent(Node camera, InputEvent @event, Vector3 eventPosition, Vector3 normal, int _shape_idx)
-    {
-        _ = camera;
-        _ = eventPosition;
-        _ = normal;
-        _ = _shape_idx;
+	private void OnBodyMouseEntered()
+	{
+		if (SelectionDisabled) return;
 
-        // Check if is mouse press :)
-        if (@event is not InputEventMouseButton mouse) return;
-        if (mouse.ButtonIndex != MouseButton.Left || !mouse.Pressed) return;
-        if (SelectionDisabled || IsSelected) return;
+		Input.SetDefaultCursorShape(Input.CursorShape.PointingHand);
+		EmitSignal(SignalName.HoverEntered);
+	}
 
-        IsSelected = !IsSelected;
-        EmitSignal(SignalName.Selected, this);
+	private void OnBodyMouseExited()
+	{
+		if (SelectionDisabled) return;
 
-        GD.Print($"Charm clicked: {CharmName}");
+		Input.SetDefaultCursorShape(Input.CursorShape.Arrow);
+		EmitSignal(SignalName.HoverExited);
+	}
 
-        // Y billboard
-        // MessageLabel.Rotation = Camera.Rotation;
-        // MessageLabel.Rotation = new(0.0f, MessageLabel.Rotation.Y, 0.0f);
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is not InputEventMouseButton mouse || mouse.ButtonMask == MouseButtonMask.Left) return;
+		if (SelectionDisabled) return;
 
-        // Store camera location
-        CameraUnsnapTransform = Camera.Transform;
-        // Snap
-        CameraSnapStartedMS = (int)Time.GetTicksMsec();
-    }
-
-    private void Wander()
-    {
-        if (Destination != Vector3.Zero && GlobalPosition.DistanceTo(Destination) > 1f) return;
-        
-        if (NavigationServer3D.MapGetIterationId(NavigationServer3D.GetMaps()[0]) == 0) return;
+		if (IsSelected)
+		{
+			CameraUnsnapStartedMS = (int)Time.GetTicksMsec();
+			EmitSignal(SignalName.Unsnapping);
+		}
+	}
 
 
-        var theta = GD.RandRange(0, Math.PI * 2);
-        var phi = GD.RandRange(0, Math.PI); // I"M NOT GREEK!  THE FUCK DOES THIS MEAN?
-        var radius = 50f;       // WHAT THE FUCK WAS THIS BULLSHIT?!
+	private void OnBodyInputEvent(Node camera, InputEvent @event, Vector3 eventPosition, Vector3 normal, int _shape_idx)
+	{
+		_ = camera;
+		_ = eventPosition;
+		_ = normal;
+		_ = _shape_idx;
 
-        var x = (float)(radius * Math.Sin(theta) * Math.Cos(phi));
-        var y = (float)(radius * Math.Sin(theta) * Math.Sin(phi));
-        var z = (float)(radius * Math.Cos(theta));
+		// Check if is mouse press :)
+		if (@event is not InputEventMouseButton mouse) return;
+		if (mouse.ButtonIndex != MouseButton.Left || !mouse.Pressed) return;
+		if (SelectionDisabled || IsSelected) return;
 
-        Destination = new Vector3(x, y, z);
-        
-        Destination = NavigationServer3D.MapGetClosestPoint(NavigationServer3D.GetMaps()[0], Destination);
-        Navigation.TargetPosition = Destination;
-    }
+		IsSelected = !IsSelected;
+		EmitSignal(SignalName.Selected, this);
 
-    private void Jump()
-    {
-        if ((int)Time.GetTicksMsec() < NextJumpMS || (JumpTween != null && JumpTween.IsRunning())) return;
+		GD.Print($"Charm clicked: {CharmName}");
 
-        NextJumpMS = (int) (Time.GetTicksMsec() + GD.RandRange(JumpIntervalMS.X, JumpIntervalMS.Y));
+		// Y billboard
+		// MessageLabel.Rotation = Camera.Rotation;
+		// MessageLabel.Rotation = new(0.0f, MessageLabel.Rotation.Y, 0.0f);
 
-        var initPos = Body.GlobalPosition;
-        var jumpDuration = GD.RandRange(JumpDurationMS.X / 1000, JumpDurationMS.Y / 1000);
+		// Store camera location
+		CameraUnsnapTransform = Camera.Transform;
+		// Snap
+		CameraSnapStartedMS = (int)Time.GetTicksMsec();
+	}
 
-        JumpTween = GetTree().CreateTween();
-        JumpTween.TweenProperty(
-            Body,
-            "position:y",
-            initPos.Y + GD.RandRange(JumpHeight.X, JumpHeight.Y),
-            jumpDuration / 2
-        ).SetTrans(Tween.TransitionType.Quad);
+	private void Wander()
+	{
+		if (Destination != Vector3.Zero && GlobalPosition.DistanceTo(Destination) > 1f) return;
+		
+		if (NavigationServer3D.MapGetIterationId(NavigationServer3D.GetMaps()[0]) == 0) return;
 
-        JumpTween.TweenProperty(
-            Body,
-            "position:y",
-            initPos.Y,
-            jumpDuration / 2
-        ).SetTrans(Tween.TransitionType.Quad);
-    }
 
-    private void SnapCamera()
-    {
-        var started = 0;
-        var unsnapping = CameraUnsnapStartedMS != 0;
+		var theta = GD.RandRange(0, Math.PI * 2);
+		var phi = GD.RandRange(0, Math.PI); // I"M NOT GREEK!  THE FUCK DOES THIS MEAN?
+		var radius = 50f;       // WHAT THE FUCK WAS THIS BULLSHIT?!
 
-        // Determine start time
-        if (IsSelected && !unsnapping)
-            started = CameraSnapStartedMS;
-        else
-            started = CameraUnsnapStartedMS;
+		var x = (float)(radius * Math.Sin(theta) * Math.Cos(phi));
+		var y = (float)(radius * Math.Sin(theta) * Math.Sin(phi));
+		var z = (float)(radius * Math.Cos(theta));
 
-        if (started == 0) return;
+		Destination = new Vector3(x, y, z);
+		
+		Destination = NavigationServer3D.MapGetClosestPoint(NavigationServer3D.GetMaps()[0], Destination);
+		Navigation.TargetPosition = Destination;
+	}
 
-        // Calculate progress
-        var transitionProgress = Math.Min(
-          ((int)Time.GetTicksMsec() - started) / CameraSnapDurationMS, 1.0  
-        );
+	private void Jump()
+	{
+		if ((int)Time.GetTicksMsec() < NextJumpMS || (JumpTween != null && JumpTween.IsRunning())) return;
 
-        // Unsnap end
-        if (CameraUnsnapStartedMS != 0 && transitionProgress >= 0.1f)
-        {
-            CameraUnsnapStartedMS = 0;
-            IsSelected = false;
-            EmitSignal(SignalName.Deselected);
-            // Commit Kuriimu
-            QueueFree();
-            return;
-        }
+		NextJumpMS = (int) (Time.GetTicksMsec() + GD.RandRange(JumpIntervalMS.X, JumpIntervalMS.Y));
 
-        // Update camera interpolation
-        if (!unsnapping)
-            Camera.GlobalTransform = Camera.GlobalTransform.InterpolateWith(CameraSnapTransform, (float)transitionProgress);
-        else
-            Camera.GlobalTransform = Camera.GlobalTransform.InterpolateWith(CameraUnsnapTransform, (float)transitionProgress);
-    }
+		var initPos = Body.GlobalPosition;
+		var jumpDuration = GD.RandRange(JumpDurationMS.X / 1000, JumpDurationMS.Y / 1000);
 
-    private void Detected()
-    {
-        Speed = 5;
-    }
+		JumpTween = GetTree().CreateTween();
+		JumpTween.TweenProperty(
+			Body,
+			"position:y",
+			initPos.Y + GD.RandRange(JumpHeight.X, JumpHeight.Y),
+			jumpDuration / 2
+		).SetTrans(Tween.TransitionType.Quad);
 
-    private void Undetected()
-    {
-        Speed = 0;
-    }
+		JumpTween.TweenProperty(
+			Body,
+			"position:y",
+			initPos.Y,
+			jumpDuration / 2
+		).SetTrans(Tween.TransitionType.Quad);
+	}
 
-    private void TransformToProp()
-    {
+	private void SnapCamera()
+	{
+		var started = 0;
+		var unsnapping = CameraUnsnapStartedMS != 0;
 
-    }
+		// Determine start time
+		if (IsSelected && !unsnapping)
+			started = CameraSnapStartedMS;
+		else
+			started = CameraUnsnapStartedMS;
 
-    private void GetFound()
-    {
+		if (started == 0) return;
 
-    }
+		// Calculate progress
+		var transitionProgress = Math.Min(
+		  ((int)Time.GetTicksMsec() - started) / CameraSnapDurationMS, 1.0  
+		);
 
-    private void ApplyBodyTexture(Texture2D texture)
-    {
-        var bodyMaterial = Body.Mesh.SurfaceGetMaterial(0).Duplicate(true) as StandardMaterial3D;
-        bodyMaterial.AlbedoTexture = texture;
-        Body.MaterialOverride = bodyMaterial;
-    }
+		// Unsnap end
+		if (CameraUnsnapStartedMS != 0 && transitionProgress >= 0.1f)
+		{
+			CameraUnsnapStartedMS = 0;
+			IsSelected = false;
+			EmitSignal(SignalName.Deselected);
+			// Commit Kuriimu
+			QueueFree();
+			return;
+		}
+
+		// Update camera interpolation
+		if (!unsnapping)
+			Camera.GlobalTransform = Camera.GlobalTransform.InterpolateWith(CameraSnapTransform, (float)transitionProgress);
+		else
+			Camera.GlobalTransform = Camera.GlobalTransform.InterpolateWith(CameraUnsnapTransform, (float)transitionProgress);
+	}
+
+	private void Detected()
+	{
+		Speed = 5;
+	}
+
+	private void Undetected()
+	{
+		Speed = 0;
+	}
+
+	private void TransformToProp()
+	{
+
+	}
+
+	private void GetFound()
+	{
+
+	}
+
+	private void ApplyBodyTexture(Texture2D texture)
+	{
+		var bodyMaterial = Body.Mesh.SurfaceGetMaterial(0).Duplicate(true) as StandardMaterial3D;
+		bodyMaterial.AlbedoTexture = texture;
+		Body.MaterialOverride = bodyMaterial;
+	}
 }
