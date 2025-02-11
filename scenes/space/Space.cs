1731fd1230eba0b79e80c255a96dc9ed3641ce56
iSneeze <yeetallthespam@gmail.com>
2025-02-12 00:06:28 +0100

    added sun, added rotation, added doom timers

--- scenes/space/Space.cs
+++ scenes/space/Space.cs
@@ -5,5 +5,16 @@ namespace CloverField.Space;
 
 public partial class Space : Node3D
 {
+	public void _on_timer_sun_hue_change_timeout(){
+		GD.Print("TIMEOUT: Sun hue change!");
+	}
+
+		public void _on_timer_end_music_start_timeout(){
+		GD.Print("TIMEOUT: End music start!");
+	}
+
+		public void _on_timer_sun_explode_timeout(){
+		GD.Print("TIMEOUT: Sun explode!");
+	}
 	
 }
